e912fe3bcf87c100aa8caf4a9f08edfe4f1736bf
amosmilimu <amosmilimu@gmail.com>
2021-10-21 11:45:02 +0300

    create splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:interintel/pages/home_page.dart';
+import 'package:interintel/utils/styles.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) => MaterialApp(
         debugShowCheckedModeBanner: false,
         title: title,
-        theme: ThemeData(primarySwatch: Colors.blue),
+        theme: ThemeData(primarySwatch: createMaterialColor(Color(0xff334759))),
         home: HomePage(),
       );
 }
